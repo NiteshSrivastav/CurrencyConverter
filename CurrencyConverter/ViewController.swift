260ff864f35810e325dcb4eff94722b055a401c9
Nitesh <niteshkmr049@gmail.com>
2018-02-08 12:21:05 +0530

    Parsing API
    In there parse API in Swift Language in a Table View with Excellent Converter

--- CurrencyConverter/ViewController.swift
+++ CurrencyConverter/ViewController.swift
@@ -8,18 +8,81 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+struct Currency : Decodable {
+    let base : String
+    let date : String
+    let rates : [String: Double]
+}
 
+class ViewController: UIViewController,UITableViewDataSource {
+    
+    @IBOutlet weak var curTF: UITextField!
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var usd : Currency?
+    var baseRate = 1.0
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tableView.dataSource = self
+//        tableView.allowsSelection = false
+//        tableView.showsVerticalScrollIndicator = false
+        self.fetchData()
+       
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func convertBtnPressed(_ sender: UIButton) {
+        if  let isGetString = curTF.text{
+            if let isDouble = Double(isGetString){
+                baseRate = isDouble
+                fetchData()
+            }
+            
+        }
+        
+        
     }
-
+    //table View Data Source Method
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if let currencyFetched = usd {
+            return currencyFetched.rates.count
+        }
+        return 0
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .value1, reuseIdentifier: nil)
+        if let currencyFetched = usd {
+            cell.textLabel?.text = Array(currencyFetched.rates.keys)[indexPath.row]
+            let selectedRate = baseRate * Array(currencyFetched.rates.values)[indexPath.row]
+            cell.detailTextLabel?.text = "\(selectedRate)"
+            return cell
+        }
+        return UITableViewCell()
+    }
+    
+    func fetchData(){
+        let url = URL(string : "https://api.fixer.io/latest?base=USD")
+        URLSession.shared.dataTask(with: url!) { (data, response, error) in
+            if url != nil{
+                do{
+                    self.usd = try JSONDecoder().decode(Currency.self,from : data!)
+                }
+                catch{
+                    print("parse Error")
+                    
+                }
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                   // print(self.usd?.base)
+                }
+                
+            }
+            else {
+                print("Error")
+            }
+        }.resume()
+    }
+    
 
 }
 
